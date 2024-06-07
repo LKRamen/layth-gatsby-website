d7ee8589684e677dedd789246a9e1e5065225880
LKRamen <78697545+LKRamen@users.noreply.github.com>
2024-06-07 10:46:31 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+const adapter = require("gatsby-adapter-netlify")
+
+module.exports = {
+    adapter: adapter({
+      excludeDatastoreFromEngineFunction: false,
+    }),
+  }
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
