34c2539718b85e8d0a24bcf221a8d2a66f68e5ad
Layth Rahman <laythrahman11@gmail.com>
2024-06-07 11:26:38 -0400

    omitted the netlify stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,6 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 
-const adapter = require("gatsby-adapter-netlify")
-
-module.exports = {
-    adapter: adapter({
-      excludeDatastoreFromEngineFunction: false,
-    }),
-  }
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
